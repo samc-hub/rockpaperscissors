c8c9d1fd729b6d85d45e4a0a8104d7b5a3cd1bb9
samc-hub <samuelcox2343@gmail.com>
2025-11-28 17:08:55 -0800

    Play through rounds of game witha single function call

--- script.js
+++ script.js
@@ -1,51 +1,70 @@
 
 
 
+let userChoice; 
+let formattedChoice;
+let humanScore = 0;
+let computerScore = 0;
+
+ //initializing array
+const stringArray = ['Rock', 'Paper', 'Scissors'];
+let randomIndex = Math.floor(Math.random() * stringArray.length);
+let randomString = stringArray[randomIndex];
 
 
-let userChoice = prompt ("Rock, Paper, Scissors, Shoot!");
-let formattedChoice = userChoice.toUpperCase(); // so text input doesn't matter 
-let humanScore = 0
-let computerScore = 0
 
-//computer choice
+function getComputerChoice() {
 const stringArray = ['Rock', 'Paper', 'Scissors'];
 const randomIndex = Math.floor(Math.random() * stringArray.length);
-const randomString = stringArray[randomIndex];
-const computerMessage = "Computer Chooses " + randomString; 
-
-
-alert(computerMessage); 
-
-
-function theGame(); {
- if (formattedChoice == "ROCK" && randomString == "Scissors") {
-  console.log("You Win! ")
-    ++humanScore;
-    --computerScore;
-  console.log("Human Score: " + humanScore + " " + "Computer Score: " + computerScore);
- } 
-else if (formattedChoice == "SCISSORS" && randomString == "Paper") {
-console.log("You Win! " + "Score: " + (1 + humanScore));
-    ++humanScore;
-    --computerScore;
-console.log("Human Score: " + humanScore + " " + "Computer Score: " + computerScore);
-} 
-else if (formattedChoice == "PAPER" && randomString == "Rock") {
-console.log("You Win! " + "Score: " + (1 + humanScore));
-    ++humanScore;
-    --computerScore;
-console.log("Human Score: " + humanScore + " " + "Computer Score: " + computerScore);
+ randomString = stringArray[randomIndex];
+let computerMessage = "Computer Chooses " + randomString; 
+alert(computerMessage);
 }
-else if (formattedChoice == randomString.toUpperCase) { 
-  console.log("No one Wins!!");
-  console.log("Human Score: " + humanScore + " " + "Computer Score: " + computerScore);
+
+function getFormattedChoice () {
+   formattedChoice = userChoice.toUpperCase();
+   return formattedChoice;
 }
-else {
-  console.log("You lose")
-    --humanScore;
-    ++computerScore;
+
+
+
+function playRound() {
+    userChoice = prompt("Rock, Paper, Scissors, Shoot!");
+  getFormattedChoice(); 
+  getComputerChoice();
+  if (formattedChoice == "ROCK" && randomString == "Scissors") {
+      alert("You Win!")
+        ++humanScore;
+        --computerScore;
+      console.log("Human Score: " + humanScore + " " + "Computer Score: " + computerScore);
+    } 
+    else if (formattedChoice == "SCISSORS" && randomString == "Paper") {
+    alert("You Win!");
+        ++humanScore;
+        --computerScore;
     console.log("Human Score: " + humanScore + " " + "Computer Score: " + computerScore);
-}
+    } 
+    else if (formattedChoice == "PAPER" && randomString == "Rock") {
+    alert("You Win!");
+        ++humanScore;
+        --computerScore;
+    console.log("Human Score: " + humanScore + " " + "Computer Score: " + computerScore);
+    }
+    else if (formattedChoice == randomString.toUpperCase()) { 
+      alert("No one Wins!!");
+      console.log("Human Score: " + humanScore + " " + "Computer Score: " + computerScore);
+    }
+    else {
+      alert("You lose")
+        --humanScore;
+        ++computerScore;
+        console.log("Human Score: " + humanScore + " " + "Computer Score: " + computerScore);
+    }
 
 }
+
+playRound();
+playRound();
+playRound();
+playRound();
+playRound();
